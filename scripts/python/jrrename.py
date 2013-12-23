470d644ef8a03de1f934961fa121f90924a441af
Johann Romero <Johann.Romero@sce.com>
2013-12-23 09:17:05 -0800

    Made some small changes to output for clarity

--- scripts/python/jrrename.py
+++ scripts/python/jrrename.py
@@ -15,14 +15,15 @@ import shutil
 def main():
     '''
     Will rename files in specified path. Change Extension or path if required
+    Change doIt flag to do dry run or to actually do work
     '''
 
     doIt = True
     cnt = 01
-    strPath = '/Volumes/Public/videos/tv/Friends/S01/'
-    currentFiles = glob.glob(strPath + '*.mp4')
+    strPath = '/Volumes/Public/videos/tv/Friends/S10/'
+    currentFiles = glob.glob(strPath + '*.avi')
     for filename in currentFiles:
-        saveName = strPath + 'Friends.S01.E' + str('%02d' % cnt) + '.mp4'
+        saveName = strPath + 'Friends.S10.E' + str('%02d' % cnt) + '.avi'
         if doIt:
             shutil.move(filename,saveName)
             print "Renaming {0} to {1}".format(filename,saveName)
